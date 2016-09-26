75a524b673419b56d28f026363a69c5a9118e931
Mohammad El-Haj <mh2x@MacBook-Pro.local>
2016-09-25 17:26:11 -0700

    Creating and saving reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,27 @@
 class ReviewsController < ApplicationController
 
-def index
-    
-end
+    def index
+        @book = Book.find(params[:book_id])
+        @reviews = @book.reviews
+    end
+
+    def new
+    	 @book = Book.find(params[:book_id])
+         @review = @book.reviews.new
+    end
+
+    def create
+         @book = Book.find(params[:book_id])
+         review_params = params.require(:review).permit(:name, :comment, :stars)
+		 @review = @book.reviews.new(review_params)
+		if @review.save
+			flash[:notice] = "Thanks for leaving me a review!"
+			redirect_to book_reviews_path(@book, @review)
+		else
+			#flash[:alert] = "You haz errors!"
+			#render :new, :alert => "Errors!"
+			render :new
+		end
+	end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
 class Review < ApplicationRecord
   belongs_to :book
+
+  #Adding validation
+  validates :name, presence: true
+  validates :comment, length: { minimum: 25}
+  
+  STARS = [1, 2, 3, 4, 5]
+
+  validates :stars, inclusion: { 
+      in: STARS, 
+      message: "must be between 1 and 5"
+    }
+
 end
